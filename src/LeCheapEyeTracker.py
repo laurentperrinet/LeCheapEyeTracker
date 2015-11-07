549969165368c8122329f4009213a0f2b6ea1ec3
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-11-07 19:51:57 -0300

    calibration : first results!

--- src/LeCheapEyeTracker.py
+++ src/LeCheapEyeTracker.py
@@ -7,9 +7,10 @@ One main file.
 
 HAARCASCADE="~/pool/libs/vision/opencv/data/haarcascades/haarcascade_frontalface_default.xml" # where to find haar cascade file for face detection
 
+import numpy as np
 from multiprocessing.pool import ThreadPool
 from collections import deque
-import cv2
+import cv2, time
 
 class StatValue:
     def __init__(self, smooth_coef = 0.5):
@@ -51,28 +52,25 @@ class Camera:
         self.last_frame_time = clock()        
         self.display = False
         self.ctime = []
+        self.eye_pos = []
         self.N = 0
-        self.face_cascade = cv2.CascadeClassifier('/Users/laurentperrinet/pool/libs/vision/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
-        self.eye_cascade = cv2.CascadeClassifier('/Users/laurentperrinet/pool/libs/vision/opencv/data/haarcascades/haarcascade_eye.xml')
+        self.cascade = cv2.CascadeClassifier('/Users/laurentperrinet/pool/science/LeCheapEyeTracker/src/haarcascade_frontalface_default.xml')
+        self.eye_template = cv2.imread('/Users/laurentperrinet/pool/science/LeCheapEyeTracker/src/my_eye.png')
+        self.wt, self.ht = self.eye_template.shape[1], self.eye_template.shape[0]
 
     def process_frame(self, frame, t0):
-
-        faces = self.face_cascade.detectMultiScale(frame, 1.3, 5) # image[, scaleFactor[, minNeighbors[, flags[, minSize[, maxSize]
-        if len(faces) > 0:
-            (x,y,w,h) = faces[0]
-            roi_color = frame[y:y+h, x:x+w]
-            frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
-            eyes = self.eye_cascade.detectMultiScale(roi_color, 1.1, 5, 4)
-            if len(eyes) == 2:
-                (ex,ey,ew,eh) = eyes[0]
-                eye_L = roi_color[ey:ey+eh, ex:ex+ew]
-                cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
-                (ex,ey,ew,eh) = eyes[1]
-                eye_R = roi_color[ey:ey+eh, ex:ex+ew]
-                cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
-                if eyes[0][0] < eyes[1][0]:
-                    eye_L, eye_R = eye_R, eye_L
-        return frame, t0
+        def get_just_one(image):
+            features, minNeighbors = [], 1
+            while len(features) == 0 and minNeighbors<20:
+                features = self.cascade.detectMultiScale(image, 1.1, minNeighbors) 
+                minNeighbors += 1
+            return features[0], minNeighbors
+        (x, y, w, h), minNeighbors = get_just_one(frame)
+        half_w, quarter_w = w//2, w//4
+        img_face = frame[(y+quarter_w):(y+quarter_w+half_w), x:x+half_w]
+        res = cv2.matchTemplate(img_face, self.eye_template, cv2.TM_CCOEFF)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        return (max_loc[0] + self.wt/2, max_loc[1] + self.ht/2), t0
 
     def grab(self):
         ret, frame = self.cap.read()
@@ -81,26 +79,28 @@ class Camera:
     def run(self, T=10):
         start = clock()
         while clock()-start <T:
-            while len(self.pending) > 0 and self.pending[0].ready():
-                res, t0 = self.pending.popleft().get()
-                self.latency.update(clock() - t0)
-                if self.display:
-                    draw_str(res, (20, 40), "latency        :  %.1f ms" % (self.latency.value*1000))
-                    draw_str(res, (20, 60), "frame interval :  %.1f ms" % (self.frame_interval.value*1000))
-                    cv2.imshow('Webcam video', res)
-                self.ctime.append(clock() - start)
-                self.N += 1
-            if len(self.pending) < self.threadn:
-                frame = self.grab()
-                t = clock()
-                self.frame_interval.update(t - self.last_frame_time)
-                self.last_frame_time = t
-                task = self.pool.apply_async(self.process_frame, (frame.copy(), t))
-                self.pending.append(task)
+            if False:
+                while len(self.pending) > 0 and self.pending[0].ready():
+                    res, t0 = self.pending.popleft().get()
+                    self.eye_pos.append([res, t0])
+                    self.latency.update(clock() - t0)
+                    self.ctime.append(clock() - start)
+                    self.N += 1
+                if len(self.pending) < self.threadn:
+                    frame = self.grab()
+                    t = clock()
+                    self.frame_interval.update(t - self.last_frame_time)
+                    self.last_frame_time = t
+                    task = self.pool.apply_async(self.process_frame, (frame.copy(), t))
+                    self.pending.append(task)
+            else:
+                res, t0 = self.process_frame (frame.copy(), clock())
+                self.eye_pos.append([res, t0])
+                
             ch = 0xFF & cv2.waitKey(1)
             if ch == 27:
                 self.close()
-        return frame
+
     
     def close(self):
         self.cap.release()
@@ -455,6 +455,75 @@ class GetHands:
             wait = max(2, (1000/FPS)-int((time.time()-t)*1000))
             cv.WaitKey(wait)
 
+
+from vispy import app
+from vispy import gloo
+
+vertex = """
+    attribute vec2 position;
+    attribute vec2 texcoord;
+    varying vec2 v_texcoord;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+        v_texcoord = texcoord;
+    }
+"""
+
+fragment = """
+    uniform sampler2D texture;
+    varying vec2 v_texcoord;
+    void main()
+    {
+        gl_FragColor = texture2D(texture, v_texcoord);
+
+        // HACK: the image is in BGR instead of RGB.
+        float temp = gl_FragColor.r;
+        gl_FragColor.r = gl_FragColor.b;
+        gl_FragColor.b = temp;
+    }
+"""
+import sys
+import atexit
+
+class Canvas(app.Canvas):
+    def __init__(self, cam, stim):
+        self.cam = cam
+        self.stim, self.timeline = stim
+        self.h, self.w, three = self.stim(0).shape
+        app.use_app('pyglet')
+        app.Canvas.__init__(self, keys='interactive', fullscreen=True, size=(1280, 960))#
+        self.program = gloo.Program(vertex, fragment, count=4)
+        self.program['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
+        self.program['texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
+        self.program['texture'] = np.zeros((self.h, self.w, 3)).astype(np.uint8)
+        width, height = self.physical_size
+        gloo.set_viewport(0, 0, width, height)
+        self._timer = app.Timer('auto', connect=self.on_timer, start=True)
+        self.start = time.time()
+        atexit.register(self.close)
+        self.show()
+
+    def on_resize(self, event):
+        width, height = event.physical_size
+        gloo.set_viewport(0, 0, width, height)
+
+    def on_draw(self, event):
+        gloo.clear('black')
+        if time.time()-self.start < self.timeline.max(): # + ret.sleep_time*2: 
+            #i_t = min(self.timeline - time.time()-self.start)
+            image = self.stim((time.time()-self.start)/self.timeline.max())
+            self.program['texture'][...] = image.reshape((self.h, self.w, 3))
+            self.program.draw('triangle_strip')
+            
+        else:
+            self.close()
+        
+    def on_timer(self, event):
+        frame = self.cam.grab()
+        res, t0 = self.cam.process_frame (frame.copy(), clock())
+        self.cam.eye_pos.append([res, t0])
+        self.update()
 
 if __name__ == '__main__':
     start = time.time()
