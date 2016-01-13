cc9b287a49c1da0fcefcb9914f283cf0a1c2e41c
David Arbib <davidarbib@yahoo.fr>
2016-01-13 16:38:20 +0100

    time to test on pi

--- dev/pi_test_fps.py
+++ dev/pi_test_fps.py
@@ -1,1 +1,23 @@
-toto
+import numpy as np
+import time
+from openRetina import PhotoReceptor
+
+def moyFPS(nb, scale):
+    N = 100
+    ctime = np.zeros(N)
+    
+    acc = 0
+    
+    for i in range(nb):
+        start = time.time()
+        
+        cam = PhotoReceptor(scale)
+        for j in range(N):
+            img = cam.grab()
+            ctime[j] = time.time()-start
+        cam.close()
+        
+        acc = acc + N/(ctime[-1]-ctime[0])
+    
+    return acc/nb
+print (moyFPS(16,8))
