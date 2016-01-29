dd2d96910ab68f64fe7741310d1ddd015c6a269f
David Arbib <davidarbib@yahoo.fr>
2016-01-29 18:59:34 +0100

    Now we have to find a way to combine multiple layers in vispy

--- src/LeCheapEyeTracker/EyeTrackerClient.py
+++ src/LeCheapEyeTracker/EyeTrackerClient.py
@@ -21,26 +21,38 @@ class Stimulation():
     A stimulation is an ensemble of stimuli and its properties. This class
     allows creating and running stimulations and is handled by Client.
 
-    To set positions we will use a coordinate system in which the origin
-    is at the top-left on the screen and coordinates are normalized.
+    To set positions we will use the non-intuitive OpenCV coordinate system in which the origin
+    is at the top-right on the screen and coordinates are normalized.
     """
 
 #----Public methods----
 
-    def __init__(self, window_h, window_w, stim_type = 'calibration'):
+    def __init__(self, window_w, window_h, stim_type = 'calibration'):
         self.stim_type = stim_type
         self.window_h = window_h
         self.window_w = window_w
         self.stim_details()
 
     def run(self):
+        """
+        Provide a continuum like use of the class
+        """
         t0 = time.time()
         i = 0
-        while (time.time()-t0 < self.duration or i <= self.nb_stim-1):
+        while (time.time()-t0 < self.duration or i <= len(self.tabPos)-1):
             if time.time()-t0 < self.transition_lag*(i+1):
-                self.draw_stimulus(self.tabPos[i])
+                image = self.draw_stimulus(self.tabPos[i])
+                #Use image as you like (Feeding a vizualisation tool)
             print ("stim en cours")
             i += 1
+
+    def get_stimulus(self, t0, t):
+        """
+        Provide a discrete use of the class
+        Return a cliche of stimulation at t, t0 the start of stimulation
+        """
+        i = int((t-t0)//self.transition_lag)
+        return self.draw_stimulus(self.tabPos[i])
 
 #----Privates methods-----
 
@@ -50,26 +62,28 @@ class Stimulation():
         """
         if self.stim_type == 'calibration':
             self.duration = 10
-            self.tabPos = [(0.5, 0.5), (0.1, 0.5), (0.5, 0.1), (0.5, 0.5), (0.9, 0.5), (0.5, 0,9), (0.5, 0.5)]
+            #Origin in OpenCV is at the right-up corner
+            self.tabPos = [(0.5, 0.5), (0.5, 0.1), (0.9, 0.5), (0.5, 0.5), (0.5, 0.9), (0.1, 0.5), (0.5, 0.5)]
             self.transition_lag = 1
             self.stimulus = 'target'
         else :
             print ("This type of stimulation is not implemented for the moment")
 
-    def draw_stimulus(self, posY, posX):
+    def draw_stimulus(self, pos):
         """
         (private) Draw a stimulus at a normalized position given.
         Compute the 'true' position.
-        posY, posX : the stimulus normalized localization
+        pos : the stimulus normalized localization
         """
         img0 = np.zeros((self.window_h,self.window_w, 3)).astype(np.uint8)
-        pos = (self.window_h*posY, self.window_w*posX)
-
+        posX, posY = pos
+        pos = (int(self.window_w*posX), int(self.window_h*posY))
         if self.stimulus == 'target':
             img = img0.copy()
-            img = cv2.circle(img, int(pos), 12, (0, 0, 255), 1)
-            img = cv2.circle(img, int(pos), 3, (0, 0, 255), -1)
+            img = cv2.circle(img, pos, 12, (0, 0, 255), 1)
+            img = cv2.circle(img, pos, 3, (0, 0, 255), -1)
 
+        return img
 #---------------------------------------------------------------------------
 
 vertex = """
@@ -101,8 +115,10 @@ class Client(app.Canvas):
         self.et = et
         self.timeline = timeline
         app.use_app('pyglet')
-        app.Canvas.__init__(self, keys='interactive', fullscreen=True)#, size=(1280, 960))#
+        app.Canvas.__init__(self, keys='interactive', fullscreen=True, size=(1280, 720))
         self.width, self.height = self.physical_size
+        print ('window size : ', self.physical_size)
+        self.stimulation = Stimulation(self.width, self.height, stim_type='calibration')
         self.program = gloo.Program(vertex, fragment, count=4)
         self.program['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
         self.program['texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
@@ -119,8 +135,11 @@ class Client(app.Canvas):
     def on_draw(self, event):
         gloo.clear('black')
         if time.time()-self.start < self.timeline.max(): # + ret.sleep_time*2:
-            image = np.random.rand(self.height, self.width, 3)*255
+            #image = np.random.rand(self.height, self.width, 3)*255
+            image = self.stimulation.get_stimulus(t0 = self.start, t = time.time())
+            #frame = self.et.cam.grab()
             self.program['texture'][...] = image.astype(np.uint8).reshape((self.height, self.width, 3))
+            #self.program['texture'] = frame
             self.program.draw('triangle_strip')
         else:
             self.close()
@@ -139,8 +158,8 @@ if __name__ == '__main__':
 
     import cv2
     import numpy as np
+    from LeCheapEyeTracker import Server
 
     fps = 100
-    stimulation = Stimulation(960, 1280)
-    screen = Client(et=None, timeline=np.linspace(0, 3., 3.*fps))
+    screen = Client(et=Server(), timeline=np.linspace(0, 7., 3.*fps))
     app.run()
